880fc6b95a44c823254a4d87ac7f38626ccdbcaf
oscar martinez <oscar01martinez@gmail.com>
2020-09-21 00:15:18 +0200

    return changed

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-   return render_template('index.html')
-   
+   return flask.send_file('index.html')
+
 if __name__ == '__main__':
    app.run()
 
